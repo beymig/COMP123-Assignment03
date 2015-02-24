b1a110d95402b3d84af7e2813397835c21dd4b74
beymig <beymig@gmail.com>
2015-02-23 22:41:59 -0500

    created new planets

--- Assignment03/Program.cs
+++ Assignment03/Program.cs
@@ -10,6 +10,11 @@ namespace Assignment03
     {
         static void Main(string[] args)
         {
+            GiantPlanet Saturno = new GiantPlanet("SATURNO",120536, 5683190000000000000,"Gas");
+            Console.WriteLine(Saturno.ToString());
+
+            TerrestrialPlanet Tierra = new TerrestrialPlanet("TIERRA", 12742, 59721900000000,true);
+            Console.WriteLine(Tierra.ToString());      
         }
     }
 }
