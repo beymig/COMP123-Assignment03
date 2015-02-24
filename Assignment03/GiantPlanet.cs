70608581633ce6dff363e398b238f094ba9558fe
beymig <beymig@gmail.com>
2015-02-23 22:13:00 -0500

    created GiantPlanet class specifications

--- Assignment03/GiantPlanet.cs
+++ Assignment03/GiantPlanet.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace Assignment03
 {
-    class GiantPlanet
+    class GiantPlanet : Planet
     {
+        ///Private Properties
+        private string _type;
+ 
+        //Constructor
+        public GiantPlanet(string name, double diameter, double mass, string type) 
+            : base(name, diameter, mass) 
+        {
+            this._type = type;
+        }
+
+        public override bool HasMoons()
+        {
+            bool hasMoons;
+            if (MoonCount > 0)
+            {
+                hasMoons = true;
+            }
+            else
+            {
+                hasMoons = false;
+            }
+            return hasMoons;
+        }
+
+        public override bool HasRings()
+        {
+            bool hasRings;
+            if (RingCount > 0)
+            {
+                hasRings = true;
+            }
+            else
+            {
+                hasRings = false;
+            }
+            return hasRings;
+        }
+  
     }
 }
