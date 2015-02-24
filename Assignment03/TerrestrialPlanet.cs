3c975e83be3da612db1ee8fefb70a4b65d321002
beymig <beymig@gmail.com>
2015-02-23 22:20:11 -0500

    created TerrestialPlanet class specifications

--- Assignment03/TerrestrialPlanet.cs
+++ Assignment03/TerrestrialPlanet.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace Assignment03
 {
-    class TerrestrialPlanet
+    class TerrestrialPlanet : Planet
     {
+         ///Private Properties
+        private bool _oxygen;
+ 
+        //Constructor
+        public TerrestrialPlanet(string name, double diameter, double mass, bool oxygen) 
+            : base(name, diameter, mass) 
+        {
+            this._oxygen = oxygen;
+        }
+
+        public override bool HasMoons()
+        {
+            bool hasMoons;
+            if (MoonCount > 0)
+            {
+                hasMoons = true;
+            }
+            else
+            {
+                hasMoons = false;
+            }
+            return hasMoons;
+        }
+
+        public override bool Habitable()
+        {
+            
+            return _oxygen;
+        }
     }
 }
