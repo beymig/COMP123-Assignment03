a957d451429f854a5b96efa1ac27bb855f76d637
beymig <beymig@gmail.com>
2015-02-23 21:20:22 -0500

    created classes and private and public properties in abstract class Planet

---
+++ Assignment03/Planet.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment03
+{
+    abstract class Planet
+    {
+        ////Private Instance Variables
+        private double _diameter;
+        private double _mass;
+        private int _moonCount;
+        private string _name;
+        private double _orbitalPeriod;
+        private int _ringCount;
+        private double _rotationPeriod;
+
+        ////Public Properties
+        public double Diameter
+        {
+            get
+            {
+                return this._diameter;
+
+            }
+
+        }
+        public double Mass
+        {
+            get
+            {
+                return this._mass;
+
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this._name;
+
+            }
+        }
+
+        public int MoonCount
+        { 
+            get
+            {
+                return this._moonCount;
+            }
+
+            set
+            {
+                this._moonCount = value;
+            }
+        }
+
+        public double OrbitalPeriod
+        {
+            get
+            {
+                return this._orbitalPeriod;
+            }
+
+            set
+            {
+                this._orbitalPeriod = value;
+            }
+        }
+
+        public int RingCount
+        {
+            get
+            {
+                return this._ringCount;
+            }
+
+            set
+            {
+                this._ringCount = value;
+            }
+        }
+
+        public double RotationPeriod
+        {
+            get
+            {
+                return this._rotationPeriod;
+            }
+
+            set
+            {
+                this._rotationPeriod = value;
+            }
+        }
+    }
+}
